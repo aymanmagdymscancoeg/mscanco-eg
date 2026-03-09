fca757c2e76e0faccec535709bc6708de0838f67
aymanmagdymscancoeg <ayman.magdy@mscanco-eg.com>
2026-03-09 11:21:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+window.addEventListener("scroll", function(){
 
+let cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+
+let position = card.getBoundingClientRect().top;
+
+let screenPosition = window.innerHeight / 1.2;
+
+if(position < screenPosition){
+
+card.style.opacity = "1";
+card.style.transform = "translateY(0)";
+
+}
+
+});
+
+});
